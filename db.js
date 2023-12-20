b45c7308d7f3e9f3c353193c0168202b9aa1227f
ayush9936 <ayush9936ru@gmail.com>
2023-12-20 17:51:19 +0530

    database

---
+++ db.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const url = "mongodb+srv://ayush121:993642@cluster0.apysa.mongodb.net/";
+
+mongoose.connect(url,)
+.then(()=>{
+    console.log("database is connected")
+})
+.catch((err)=>{
+    console.error(err)
+})
+
+module.exports = mongoose;
+
+
+
+
+// const {MongoClient}= require('mongodb')
+// const uri="mongodb://localhost:27017/Hospital"
+
+// const client = new MongoClient(uri,)
+
+// async function connectTodb() {
+//     try{
+//         await client.connect();
+//         console.log("connected")
+
+//     }catch(err){
+//         console.error("error")
+//     }
+    
+// }
+
+// connectTodb();
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express= require("express")
+require("./db")
 const app= express()
 
 const port = 4000;
